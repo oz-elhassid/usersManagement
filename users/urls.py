a6df5138b0b70e5d2ede014914ffa9ad9e8e9c34
oz <ozel@post.bgu.ac.il>
2019-06-04 23:14:43 +0300

    added a basic login and welcome page

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.welcome, name='welcome'),
+    path('welcome', views.welcome, name='welcome'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 
+@login_required
 def welcome(request):
-    return HttpResponse("a welcome page placeholder")
+    return HttpResponse("welcome back %s." % request.user.get_username())
