41e8ccb11402bd9b5eeb40c75d1cc687e3e916b1
oz <ozel@post.bgu.ac.il>
2019-06-04 14:48:26 +0300

    edited settings to use mysql, and added requirements.txt

--- usersManagement/settings.py
+++ usersManagement/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'usersManagement.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydatabase',
+        'USER': 'oz',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
@@ -106,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Jerusalem'
 
 USE_I18N = True
 
