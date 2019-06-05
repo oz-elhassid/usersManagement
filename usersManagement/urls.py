ea83327b4b58fb84cb9ffbb200856ca5afa8e4f5
oz <ozel@post.bgu.ac.il>
2019-06-05 10:02:29 +0300

    changed the default page

--- usersManagement/urls.py
+++ usersManagement/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth import login
 from django.urls import include, path
 
 urlpatterns = [
-    path('users/', include('users.urls')),
+    path('', include('users.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls'))
 ]
